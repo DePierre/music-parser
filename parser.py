d6289bcd01a67c74bd66cebff96a7ebaf3335885
nop <pascal.rito@gmail.com>
2011-07-21 22:57:05 +0200

    new version of parser

--- parser.py
+++ parser.py
@@ -1,54 +1,105 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
-def getProperString(oldString):
-    properString = oldString.encode('ascii', 'replace')
-    properString = properString.title()
-                    
-    return properString
-                    
-def getTrackName(track, number = ''):
+import unicodedata
+from file_types import *
+
+def file_parser(file):
+    """
+    Global function to parse the new filename of the music file
+    Use the properties of file_types to create:
+        -the new name of the file
+        -the new path of the file
+    Return a file_types object which contains those datas
+    """
+
+    # Copy of the file instance to save it
+    new_file = file
+    # We find the right function depending on the extension of the file
+    meta_func = find_meta_function(find_extension(file))
+    return meta_func(new_file)
+    
+def find_extension(file):
+    """
+    Find the extension of the file.
+    Return a string like 'mp3', 'wav', ...
+    """
+
+    index_ext = file.name.rfind('.')
+    if index_ext != -1:
+        return file.name[index_ext+1:]
+    # else: we raise an exception because
+    # we can't find any extension
+
+def find_meta_function(ext):
+    """
+    Find the right set_meta_ext function if exist
+    Return the name of the function as a string
+    """
+
+    func_name_key = 'set_meta_' + ext
+    return globals()[func_name_key]
+    # else: we raise an exception because
+    # we don't know the format of the music file
+
+def format_string(str):
+    """
+    Set special char 'éèêëàâäîï...' as 'eeeeaaaii...'
+    and use str.title to format the string like a title.
+    (i.e 'the wall' => 'The Wall')
+    Return an ansi encoded string
+    """
+
+    return unicodedata.normalize('NFKD', unicode(str)).encode('ascii', 'ignore').title()
+
+
+def build_path(elements):
+    """
+    Build the final path following this way:
+        elements1 + '/' + elements2 + '/' + ...
+    Return the fullpath as a string
+    """
+
+    fullpath = ''
+    for elem in elements:
+        fullpath += elem
+        fullpath += '/'
+    return fullpath[:-1]
+
+def build_track_name(track, number = ''):
+    """
+    Build the track name with or without tracknumber.
+    For instance :
+        -track = 'Beat It'
+        -number = '01'
+        -trackname = '01 - Beat It'
+    Return the trackname as a string
+    """
+
     name = ''
     if number:
         name += number
         name += ' - '
     name += track
-                     
     return name
-                                
-def getPath(elements):
-    fullpath = ''
-    for elem in elements:
-        fullpath += elem
-        fullpath += '/'
-    fullpath = fullpath.rstrip('/')
-    
-    return fullpath
-                      
-from id3.ID3 import *
-from file_types import *
 
-class MusicFileParser(list):
+def set_meta_mp3(file):
     """
-    blabla
+    Set new values of the file using tag id3
+    This method uses different ones,
+    to create the new values.
+    Modifies the file with new values
     """
-                                                        
-    def __init__(self, file):
-        self.filename = getProperString(file.name)
-        self.path = file.path.encode('ascii', 'replace')
-        self.path = self.path.rstrip('/')
-                                                                        
-class MusicFileParser_mp3(MusicFileParser):
-    """
-    blabla
-    """
-                                                                                        
-    def __init__(self, file):
-        MusicFileParser.__init__(self, file)
-        self.artist = getProperString(file.tag["ARTIST"])
-        self.album = getProperString(file.tag["ALBUM"])
-        #try:
-        #self.name = getTrackName(file.tag["TITLE"], file.tag["TRACKNUMBER"])
-        #finally:
-        #self.name = getTrackName(file.tag["TITLE"])
-        self.name = getTrackName(file.tag["TITLE"])     
-        self.fullpath = getPath((self.path, self.artist, self.album, self.name))
+
+    file.tag["TITLE"] = format_string(file.tag["TITLE"])
+    file.tag["ARTIST"] = format_string(file.tag["ARTIST"])
+    file.tag["ALBUM"] = format_string(file.tag["ALBUM"])
+    number = ''
+    if "TRACKNUMBER" in vars(file.tag):
+        number = file.tag["TRACKNUMBER"]
+    else:
+        pass
+    file.name = build_track_name(file.tag["TITLE"], number) + '.mp3'
+    file.path = build_path([file.tag["ARTIST"], file.tag["ALBUM"]])
+    return file
+
