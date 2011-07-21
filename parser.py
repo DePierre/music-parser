0895d95c9eef5d7cb38a5ce37b8132ee127b975a
nop <pascal.rito@gmail.com>
2011-07-22 00:31:27 +0200

    parser returns a dict now

--- parser.py
+++ parser.py
@@ -10,14 +10,15 @@ def file_parser(file):
     Use the properties of file_types to create:
         -the new name of the file
         -the new path of the file
-    Return a file_types object which contains those datas
+    Return a dict which contains those datas
     """
 
     # Copy of the file instance to save it
     new_file = file
     # We find the right function depending on the extension of the file
     meta_func = find_meta_function(find_extension(file))
-    return meta_func(new_file)
+    dict_file = meta_func(new_file)
+    return dict_file
     
 def find_extension(file):
     """
@@ -88,18 +89,33 @@ def set_meta_mp3(file):
     Set new values of the file using tag id3
     This functions uses different ones,
     to create the new values.
-    Return the new file with new values
+    Return a dict with new values
     """
 
-    file.tag["TITLE"] = format_string(file.tag["TITLE"])
-    file.tag["ARTIST"] = format_string(file.tag["ARTIST"])
-    file.tag["ALBUM"] = format_string(file.tag["ALBUM"])
-    number = ''
-    if "TRACKNUMBER" in vars(file.tag):
-        number = file.tag["TRACKNUMBER"]
+    dict_file = {}
+    if file.tag.d.has_key("TITLE"):
+        dict_file['title'] = format_string(file.tag["TITLE"])
     else:
-        pass
-    file.name = build_track_name(file.tag["TITLE"], number) + '.mp3'
-    file.path = build_path([file.tag["ARTIST"], file.tag["ALBUM"]])
-    return file
+        dict_file['title'] = 'Unknow'
+    if file.tag.d.has_key("ARTIST"):
+        dict_file['artist'] = format_string(file.tag["ARTIST"])
+    else:
+        dict_file['artist'] = 'Unknow'
+    if file.tag.d.has_key("ALBUM"): 
+        dict_file['album'] = format_string(file.tag["ALBUM"])
+    else:
+        dict_file['artist'] = 'Unknow'
+    number = ''
+    if file.tag.has_key("TRACKNUMBER"):
+        number = file.tag["TRACKNUMBER"]
+    dict_file['tracknumber'] = number
+    dict_file['name'] = build_track_name(dict_file['album'], number) + '.mp3'
+    dict_file['path'] = build_path([dict_file['artist'], file.tag["ALBUM"]])
+    if file.tag.d.has_key("YEAR"):
+        dict_file['year'] = file.tag["YEAR"]
+    if file.tag.d.has_key("COMMENT"):
+        dict_file['comment'] = file.tag["COMMENT"]
+    if file.tag.d.has_key("GENRE"):
+        dict_file['genre'] = file.tag["GENRE"]
+    return dict_file
 
