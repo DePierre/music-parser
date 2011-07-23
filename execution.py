ae6aab3f536d8b757052356be740dfc8836f3f39
nop <pascal.rito@gmail.com>
2011-07-23 21:28:10 +0200

    correction of mistake

--- execution.py
+++ execution.py
@@ -16,7 +16,7 @@ def execution(filebefore, dict_file_after):
     del dict_tag['path']
     # We define the new tag
     for key in dict_file_after:
-        filebefore.tag[key] = dict_file_after[key]
+        filebefore.tag[key] = dict_tag[key]
     # 2ndly, we rename the file and move it
     if not Settings.quiet:
         print filebefore.path + filebefore.name + ' moved to ' + dict_file_after['path'] + dict_file_after['name']
